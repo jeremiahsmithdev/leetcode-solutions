221b5f2849a39e192c23f9b7914ae632ba1127a9
Jeremiah Smith <31682520+psygypsie@users.noreply.github.com>
2020-10-08 01:43:23 +1100

    update 4.median-of-two-sorted-arrays.py

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -5,23 +5,13 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-
         merged = nums1 + nums2
-        # print(merged)
         merged = sorted(merged)
-        # print(merged)
         if len(merged) % 2 == 1:
             median = merged[int(len(merged) / 2)]
-            # print(median)
         else:
             median = float(
             merged[int((len(merged) / 2) - 1)] +
             merged[(int(len(merged) / 2))]
             ) / 2
-            # print(median)
         return(median)
-
-
-
-        
-    print(findMedianSortedArrays(0, [1,2], [3,4]))
